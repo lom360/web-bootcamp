17725a6efacaa362c8e9e1fe1504f973fa062440
Sarom Thin <lom360@gmail.com>
2018-03-16 22:08:29 +0000

    Practiced routings

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 
+// Order of routes matter. The first route that matches
+// will be the only route that runs. So if the below route
+// runs first, then the other routes afterwards will not run.
+// app.get("*", function(req,res){
+//     res.send("You are a star");
+// });
 
 // "/" => "Hi there"!
 app.get("/", function(req,res){
@@ -14,6 +20,21 @@ app.get("/bye", function(req,res){
 app.get("/dog", function(req,res){
     console.log("SOMEONE MADE A REQUEST TO /DOG");
     res.send("MEOW!!!"); 
+});
+
+app.get("/r/:subredditName", function(req,res){
+    console.log(req.params);
+    res.send("Welcome to a subreddit!!!");
+});
+
+app.get("/r/:subreddit/comments/:id/:title",function(req,res){
+    var subreddit = req.params.subreddit
+    //The ':' tells express to not match name for name but patterns instead.
+    res.send("Welcome to the " + subreddit.toUpperCase() + " subreddit!"); 
+});
+
+app.get("*", function(req,res){
+    res.send("You are a star");
 });
 
 
