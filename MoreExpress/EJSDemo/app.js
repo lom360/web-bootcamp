be0087372d09ad0b970a7ebd9565719ced5abe87
Sarom Thin <lom360@gmail.com>
2018-03-18 09:53:23 +0000

    More ejs practice.

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -9,7 +9,16 @@ app.get("/",function(req,res){
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs",{thingVAr: thing});
+    res.render("love.ejs",{thingVar: thing});
+});
+
+app.get("/posts", function(req,res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My adorable pet bunny", author: "Charlie"},
+        {title: "Can you believe this pomsky?", author: "Colt"}
+    ];
+    res.render("posts.ejs", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
