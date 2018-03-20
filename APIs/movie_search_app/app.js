d75e92ece7441a53f5d076b2542358700770833a
Sarom Thin <lom360@gmail.com>
2018-03-20 21:35:59 +0000

    Called a list from api

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -5,11 +5,13 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+app.set("view engine","ejs");
+
 app.get("/results", function(req,res){
-    request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+    request("http://omdbapi.com/?s=new%20york&apikey=thewdb", function(error, response, body){
         if(!error && response.statusCode == 200){
-            var results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]);
+            var data = JSON.parse(body);
+            res.render("results",{data:data});
         }
     });
 });
