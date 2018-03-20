ea65d9183dccb542fad64c72796a969f2b2978d9
Sarom Thin <lom360@gmail.com>
2018-03-20 10:47:34 +0000

    Practiced more posting

--- MoreExpress/PostRequestDemo/app.js
+++ MoreExpress/PostRequestDemo/app.js
@@ -1,20 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lilly"];
 
 app.get('/', function(req,res){
     res.render('home');
 });
 
+app.post("/addfriend", function(req, res){// 'post' is used to send data e.g. servers or back end.
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+});
+
 app.get('/friends',function(req,res){// 'get' is used to retrieve data.
-    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lilly"];
     res.render('friends',{friends:friends});
 });
 
-app.post("/addfriend", function(req, res){// 'post' is used to send data e.g. servers or back end.
-    res.send("You have reached a post route!!!");
-});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has been called!!!");
