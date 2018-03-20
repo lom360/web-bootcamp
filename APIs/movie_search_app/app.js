b259faa68f1f335238668f915f10de846a07a008
Sarom Thin <lom360@gmail.com>
2018-03-20 21:13:50 +0000

    Created a small movie app that calls an api

---
+++ APIs/movie_search_app/app.js
@@ -0,0 +1,20 @@
+//omdbapi.com
+//api key: &apikey=thewdb
+
+var express = require('express');
+var app = express();
+var request = require('request');
+
+app.get("/results", function(req,res){
+    request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var results = JSON.parse(body);
+            res.send(results["Search"][0]["Title"]);
+        }
+    });
+});
+
+
+app.listen(process.env.PORT, process.env.IP,function(){
+    console.log("Movie app has been called.");
+});
