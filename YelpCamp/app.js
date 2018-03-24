810135d22a2c458b8740618948cdc23d3e75bc79
Sarom Thin <lom360@gmail.com>
2018-03-24 10:05:25 +0000

    added a new route

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -10,28 +10,31 @@ app.set("view engine","ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema); // Converting the schema into a piece of code where we'll be able to use methods.
 
-// Campground.create({
-//     name: "Granite Hill", 
-//     image: "https://farm6.staticflickr.com/5723/21955780899_9c030cd802.jpg"
-// }, function(err, campground){
-//     if(err){
-//         console.log(err);
-//     }else{
-//         console.log("Newly Created Campground");
-//         console.log(campground);
-//     }
-// });
+Campground.create({
+    name: "Granite Hill", 
+    image: "https://farm6.staticflickr.com/5723/21955780899_9c030cd802.jpg",
+    description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
+}, function(err, campground){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Newly Created Campground");
+        console.log(campground);
+    }
+});
 
 
 app.get('/',function(req,res){
     res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get('/campgrounds', function(req,res){
         // Get all campgrounds from DB
         Campground.find({}, function(err,allCampgrounds){
@@ -44,6 +47,7 @@ app.get('/campgrounds', function(req,res){
         // res.render("campgrounds",{campgrounds:campgrounds});
 });
 
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res){
     // get dat from form and add to campgrounds array.
     var name = req.body.name;
@@ -61,10 +65,28 @@ app.post("/campgrounds", function(req, res){
     res.redirect("/campgrounds");
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new",function(req,res){
     res.render("new.ejs");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    // render show template with that campground
+    res.send("THIS WILL BE THE SHOW PAGE ONE DAY");
 });
 
 app.listen(process.env.PORT, process.env.IP,function(){
     console.log("Yelp Camp At Your Service!!!");
 });
+
+
+
+/*
+ * name        url           verb      description
+ *===============================================================
+ * INDEX      /dogs          GET      Display a list of all dog
+ * NEW        /dogs/new      GET      Display a form to make a new dog
+ * CREATE     /dogs          POST     Add new dog to DB
+ * SHOW       /dogs/:id      GET      Shows info about one dog
+ */
