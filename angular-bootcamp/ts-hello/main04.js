fd0bf0231110b014d105542a8534c4ec8cefb318
lom360 <lom360@mail.fresnostate.edu>
2018-03-27 16:55:43 -0700

    Created arrow function

---
+++ angular-bootcamp/ts-hello/main04.js
@@ -0,0 +1,7 @@
+var log = function (message) {
+    console.log(message);
+};
+// Arrow function
+var doLog = function (message) {
+    console.log(message);
+};
---
+++ angular-bootcamp/ts-hello/main04.ts
@@ -0,0 +1,8 @@
+let log = function(message) {
+    console.log(message);
+}
+
+// Arrow function
+let doLog = (message) => {
+    console.log(message);
+}
