a73b0ef86370d25506bdfb88c29232cd6562d6f2
lom360 <lom360@mail.fresnostate.edu>
2018-05-08 20:42:45 -0700

    Learned about string interpolation and data binding.

--- angular-bootcamp/03-angular_fundamentals/hello-world/src/app/course/course.component.ts
+++ angular-bootcamp/03-angular_fundamentals/hello-world/src/app/course/course.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 // A component incapsulates the data, logic and html markup for view.
 @Component({
   selector: 'app-courses', //This is the tag name we'll use to call this componenet
-  template: '<h2>{{ title }}</h2>',// We will either call an html file or actually write up html code.
+  // The double curly braces is used to render data on page. It's called "data binding" or string interpolation.
+  template: '<h2>{{ this.title }}</h2>',// We will either call an html file or actually write up html code.
   styleUrls: ['./course.component.css'] // This will call the css file that will be used for styling.
 })
 export class CourseComponent implements OnInit {
@@ -14,4 +15,7 @@ export class CourseComponent implements OnInit {
   ngOnInit() {// This is used for interface. We will learn this later on.
   }
 
+  getTitle() {
+    return this.title;
+  }
 }
