e5a319c10aaa2182fe02030d4128f65fc0085d71
lom360 <lom360@mail.fresnostate.edu>
2018-05-08 21:33:35 -0700

    Learned about dependency injection

--- angular-bootcamp/03-angular_fundamentals/hello-world/src/app/course/course.component.ts
+++ angular-bootcamp/03-angular_fundamentals/hello-world/src/app/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../courses.service';
 
 // A component incapsulates the data, logic and html markup for view.
 @Component({
@@ -24,7 +25,14 @@ export class CourseComponent implements OnInit {
   title = "List of courses";
   courses;
 
-  constructor() { }
+  //By adding that parameter in the constructor.
+  // What will happen is that "service" will be instantiated by "CoursesServcie".
+  // This is called dependency injection.
+  constructor(service: CoursesService) { 
+    // let service = new CoursesService(); We don't use this method because it can affect the parameters of other instantiation.
+    //Using the method getCourses() to set the value of courses. Which is an array of generic courses.
+    this.courses = service.getCourses();
+  }
 
   ngOnInit() {// This is used for interface. We will learn this later on.
   }
