3e985fdd40ff8a6a927b9d162df66d80ce19410a
lom360 <lom360@mail.fresnostate.edu>
2018-05-08 19:56:21 -0700

    Got a better idea about importing and exporting components

--- angular-bootcamp/03-angular_fundamentals/hello-world/src/app/course/course.component.ts
+++ angular-bootcamp/03-angular_fundamentals/hello-world/src/app/course/course.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+// A component incapsulates the data, logic and html markup for view.
 @Component({
-  selector: 'app-course', //This is the tag name we'll use to call this componenet
-  templateUrl: './course.component.html',// We will either call an html file or actually write up html code.
+  selector: 'app-courses', //This is the tag name we'll use to call this componenet
+  template: '<h2>{{ title }}</h2>',// We will either call an html file or actually write up html code.
   styleUrls: ['./course.component.css'] // This will call the css file that will be used for styling.
 })
 export class CourseComponent implements OnInit {
+  title = "List of courses";
 
   constructor() { }
 
