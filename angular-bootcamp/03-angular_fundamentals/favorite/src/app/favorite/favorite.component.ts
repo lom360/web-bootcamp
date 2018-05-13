b915f9f46c55f6420830abb8b0346ae3cc8c498f
lom360 <lom360@mail.fresnostate.edu>
2018-05-13 01:22:22 -0700

    Added instructors solution to the problem.

--- angular-bootcamp/03-angular_fundamentals/favorite/src/app/favorite/favorite.component.ts
+++ angular-bootcamp/03-angular_fundamentals/favorite/src/app/favorite/favorite.component.ts
@@ -6,25 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-  isClicked = true;
-  myClass;
+  // All the commented lines is a part of the solution I made for the assignment.
+  // isClicked = true;
+  // myClass;
+  isFavorite: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onToggle() {
-    this.isClicked = !this.isClicked;
-  }
+  // onToggle() {
+  //   this.isClicked = !this.isClicked;
+  // }
 
-  setGlyphicon() {
-    if(this.isClicked) {
-      this.myClass = "glyphicon-star";
-      return this.myClass;
-    }else{
-      this.myClass = "glyphicon-star-empty";
-      return this.myClass;
-    }
+  // setGlyphicon() {
+  //   if(this.isClicked) {
+  //     this.myClass = "glyphicon-star";
+  //     return this.myClass;
+  //   }else{
+  //     this.myClass = "glyphicon-star-empty";
+  //     return this.myClass;
+  //   }
+  // }
+
+  onClick() {
+    this.isFavorite = !this.isFavorite;
   }
 }
