56847b2517db6c659c972c5dd985cc87a795872a
lom360 <lom360@mail.fresnostate.edu>
2018-05-13 01:04:20 -0700

    Used ngClass and (click) to toggle a solid star and empty star

--- angular-bootcamp/03-angular_fundamentals/favorite/src/app/favorite/favorite.component.ts
+++ angular-bootcamp/03-angular_fundamentals/favorite/src/app/favorite/favorite.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
+  isClicked = true;
+  myClass;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onToggle() {
+    this.isClicked = !this.isClicked;
+  }
+
+  setGlyphicon() {
+    if(this.isClicked) {
+      this.myClass = "glyphicon-star";
+      return this.myClass;
+    }else{
+      this.myClass = "glyphicon-star-empty";
+      return this.myClass;
+    }
+  }
 }
