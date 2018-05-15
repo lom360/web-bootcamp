1672b843f8f0ef1078736b67ee24d13f9d8f2601
lom360 <lom360@mail.fresnostate.edu>
2018-05-15 15:33:49 -0700

    Refactored code to make it look neater

--- angular-bootcamp/03-angular_fundamentals/title-case/src/app/title-case.pipe.ts
+++ angular-bootcamp/03-angular_fundamentals/title-case/src/app/title-case.pipe.ts
@@ -8,20 +8,35 @@ export class TitleCasePipe implements PipeTransform {
   transform(value: string): any {
     if (!value) return null;
   
+
+
+    let words = value.split(' '); //splits the string whenever it encounters a space or ' '.
+    for (var i = 0; i < words.length; i++){
+
+      let word = words[i];
+      // i > 0 is to take into account if the preposition is the first word of the title
+      // isPreposition is a method created below.
+      if(i > 0 && this.isPreposition(word))
+        word = word.toLowerCase();
+      else
+        word = this.toTitleCase(word);
+    }
+
+    return words.join(' ');
+  }
+
+  // Method to capitalize first letter and lowercase any following letters
+  private toTitleCase(word: string): string {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+  }
+
+  // This private method is used to determine if a word is a preposition or not.
+  private isPreposition(word: string): boolean {
     let prepositions = [
       'of',
       'the'
     ];
 
-    let words = value.split(' ');
-    for (var i = 0; i < words.length; i++){
-      if(prepositions.includes(words[i])){
-        words[i] = words[i].toLowerCase();
-      }else{
-        words[i] = words[i].substr(0,1).toUpperCase() + words[i].substr(1).toLowerCase();
-      }
-    }
-    
-    return words.join(' ');
+    return prepositions.includes(word.toLowerCase());
   }
 }
