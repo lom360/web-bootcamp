90b17a765aaace9751ab0550512658a208b74bde
lom360 <lom360@mail.fresnostate.edu>
2018-05-15 14:21:06 -0700

    Started forms title assignment

---
+++ angular-bootcamp/03-angular_fundamentals/title-case/src/app/title-form/title-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-form',
+  templateUrl: './title-form.component.html',
+  styleUrls: ['./title-form.component.css']
+})
+export class TitleFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
