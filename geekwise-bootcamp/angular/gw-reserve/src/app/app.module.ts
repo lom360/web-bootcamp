d9a90aabc1fec03f17fc9df61e53e50026d35a82
lom360 <lom360@mail.fresnostate.edu>
2018-05-15 18:13:13 -0700

    Restarting gw-reserve project

--- geekwise-bootcamp/angular/gw-reserve/src/app/app.module.ts
+++ geekwise-bootcamp/angular/gw-reserve/src/app/app.module.ts
@@ -1,55 +1,16 @@
-import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Route } from "@angular/router";
- 
-import { AppComponent } from './app.component';
-import { WelcomeApp } from './welcome/welcome.component';
-import { NavComponent } from './navigation/navigation.component';
-import { FourohfourComponent } from './fourohfour/fourohfour.component';
-import { AboutComponent } from './about/about.component';
-import { RoomComponent } from './room/room.component';
 
-const routes: Route[] = [
-  {
-    component: WelcomeApp,
-    path: 'welcome'
-  },
-  {
-    component: AboutComponent,
-    path: 'about'
-  },
-  {
-    component: WelcomeApp,
-    path: ""
-  },
-  {
-    component: RoomComponent,
-    path: "room/:id"
-  },
-  {
-    path: "",
-    redirectTo: 'welcome',
-    pathMatch: "full"
-  },
-  {
-    path: "**",
-    component: FourohfourComponent
-  }
-];
+
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    WelcomeApp,
-    FourohfourComponent,
-    AboutComponent,
-    RoomComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
