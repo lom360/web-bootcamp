26ba48f299f448f22a5ff1110cfd2fcbb61dd318
lom360 <lom360@mail.fresnostate.edu>
2018-05-17 22:57:25 -0700

    Applied @input() property

--- angular-bootcamp/05-reusable_components/hello-world/src/app/favorite/favorite.component.ts
+++ angular-bootcamp/05-reusable_components/hello-world/src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  isFavorite: boolean;
+  @Input() isFavorite: boolean;
 
   constructor() { 
-    this.isFavorite = false;
+    
   }
 
   ngOnInit() {
