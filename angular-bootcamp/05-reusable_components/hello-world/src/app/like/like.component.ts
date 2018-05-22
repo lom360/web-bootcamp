b8ab00c80ebfa85ae64c6d66756cd125682f2914
lom360 <lom360@mail.fresnostate.edu>
2018-05-21 23:13:45 -0700

    Created like component

--- angular-bootcamp/05-reusable_components/hello-world/src/app/like/like.component.ts
+++ angular-bootcamp/05-reusable_components/hello-world/src/app/like/like.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-like',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent implements OnInit {
+  @Input('isActive') isSelected: boolean;
+  @Input('likesCount') count: number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick(){
+    this.isSelected = !this.isSelected;
+    
+    //If the like button is pressed then count will be 1
+    //If it is unpressed then count will be 0.
+    this.count = (this.count > 0) ? 0 : 1;
+  }
 }
