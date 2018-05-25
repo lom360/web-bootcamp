3c9930ec5e94be11b0743fef26625d3132e18752
lom360 <lom360@mail.fresnostate.edu>
2018-05-24 21:19:40 -0700

    Went over change detection

--- angular-bootcamp/06-directives/hello-world/src/app/app.component.ts
+++ angular-bootcamp/06-directives/hello-world/src/app/app.component.ts
@@ -31,4 +31,13 @@ export class AppComponent {
     { id: 1, name: 'course2 '},
     { id: 1, name: 'course3 '},
   ];
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4' });
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 }
