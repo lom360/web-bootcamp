f1e91786c434eb16f7363c7a8497253f172d955a
lom360 <lom360@mail.fresnostate.edu>
2018-06-06 02:39:45 -0700

    Practiced the basics with variables, strings, prompt and console.log

---
+++ fullstack-bootcamp/06-IntroJS/stalker.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age");
+
+var fullName = firstName + " " + lastName;
+
+console.log("Your full name is " + fullName + ".");
+console.log("Your are " + userAge + " years old" + ".");
