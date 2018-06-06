13c9323ae4b1b6377f50e016600c63512587b474
lom360 <lom360@mail.fresnostate.edu>
2018-06-06 02:55:55 -0700

    Practiced more js by creating age calculator

---
+++ fullstack-bootcamp/06-IntroJS/age.js
@@ -0,0 +1,6 @@
+var userAge = prompt("What is your age? We'll tell you how many days you have lived.");
+
+
+var totalDays = userAge * 365.25;
+
+alert("You have lived for a total of " + totalDays);
