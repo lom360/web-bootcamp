5b9df9a2b544d98106d69379a5d1746f56190ba8
lom360 <lom360@mail.fresnostate.edu>
2018-06-07 00:13:52 -0700

    Completed conditional exercise

--- fullstack-bootcamp/07-ControlFlowJS/script.js
+++ fullstack-bootcamp/07-ControlFlowJS/script.js
@@ -12,3 +12,28 @@ var msg = "haha!";
 var isFunny = "false";
 
 console.log(!(( str || msg ) && isFunny));
+
+
+
+
+
+//////////////////////////////
+// Conditional
+
+var age = 25;
+
+if(age < 0) {
+    console.log("error");
+}else if(age < 18) {
+    console.log("Sorry, you are not old enough to enter the venue.");
+}else if (age == 21) {
+    console.log("Happy 21st birthday!!!");
+}else if(Math.sqrt(age)%1 == 0) {
+    console.log("Your age is a perfect square");
+}else if (age%2 != 0) {
+    console.log("Your age is odd!");
+}else if(age < 21) {
+    console.log("You can enter, but cannot drink.");
+}else {
+    console.log("Come on in. You can drink.");
+}
