64f0c59c313d7b692d40fbf7c6a67616dfb21b4a
lom360 <lom360@mail.fresnostate.edu>
2018-06-08 22:19:47 -0700

    Included solutions from instructor

--- fullstack-bootcamp/08-FunctionsJS/function.js
+++ fullstack-bootcamp/08-FunctionsJS/function.js
@@ -1,11 +1,17 @@
 // Write a function that determines if a number is even with true/false.
 function isEven(num) {
-    if(num % 2 === 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    // if(num % 2 === 0) {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
+
+    // Since we are returning true when the statment is true
+    // and returning false when the statement is false,
+    // then we can go ahead and just return the boolean logic
+    // to shorten the code.
+    return num % 2 === 0;
 }
 
 
@@ -20,25 +26,35 @@ function factorial(num) {
 
 // This is another function to evaluate factorial. It's just in a loop version.
 function facts(num) {
-    let answer = num;
-    while(num > 1) {
-        answer = answer * (num - 1);
+    // Define a result variable
+    let result = 1;
+
+    // Calculate factorial and store value in result
+    while(1 < num) {
+        result = result * num;
         num--;
     }
-    return answer;
+
+    // Return the result variable
+    return result;
 }
 
 // A function that converts kebab-case to snake_case.
 function kebabToSnake(str) {
-    var count = 0;
-    for(var i = 0; i < str.length; i++) {
-        if(str[i] == "-") {
-            count++;
-        }
-    }
-    while(count >= 0) {
-        str = str.replace("-","_");
-        count--;
-    }
-    return str;
+    // My solution below
+    // var count = 0;
+    // for(var i = 0; i < str.length; i++) {
+    //     if(str[i] == "-") {
+    //         count++;
+    //     }
+    // }
+    // while(count > 0) {
+    //     str = str.replace("-","_");
+    //     count--;
+    // }
+    // return str;
+
+    // This is the instructor's solution
+    var newStr = str.replace(/-/g, "_");
+    return newStr;
 }
