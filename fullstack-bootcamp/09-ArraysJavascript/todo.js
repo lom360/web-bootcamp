f9374f64a4ce4bdcf950b6a5f4f011378164fe8c
lom360 <lom360@mail.fresnostate.edu>
2018-06-09 23:31:48 -0700

    Creating simple to do list app

---
+++ fullstack-bootcamp/09-ArraysJavascript/todo.js
@@ -0,0 +1,21 @@
+window.setTimeout(function () {
+    
+    var todos = ["Buy New Turtle"];
+
+    var input = prompt("What would you like to do?");
+
+    while(input != "quit") {
+        // handle input
+        if (input === "list") {
+            console.log(todos);
+        } else if (input === "new") {
+            // ask for new todo
+            var newTodo = prompt("Enter new todo");
+            todos.push(newTodo);
+        }
+
+        // ask again for new input
+        input = prompt("What would you like to do?");
+    }
+    console.log("OK, YOU QUIT THE APP");
+}, 500);
