44e6e650e94f85bc49558f679a37e8966ade88d1
lom360 <lom360@mail.fresnostate.edu>
2018-06-13 14:47:27 -0700

    Used seven different ways to select the first p tag

---
+++ fullstack-bootcamp/11-DOM/dom.js
@@ -0,0 +1,21 @@
+var p1 = document.getElementById("first");
+
+var p2 = document.getElementsByClassName("special")[0];
+
+var p3 = document.querySelector(".special");
+
+var p4 = document.querySelector("#first");
+
+var p5 = document.querySelectorAll(".special")[0];
+
+var p6 = document.querySelector("body p.special");
+
+var p7 = document.querySelector("body p#first");
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+console.log(p4);
+console.log(p5);
+console.log(p6);
+console.log(p7);
