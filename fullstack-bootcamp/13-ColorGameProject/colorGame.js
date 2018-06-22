ef45ef227329283fd558fa80293087f259b6324b
lom360 <lom360@mail.fresnostate.edu>
2018-06-21 23:39:29 -0700

    Used random function to radomly select color to search for

--- fullstack-bootcamp/13-ColorGameProject/colorGame.js
+++ fullstack-bootcamp/13-ColorGameProject/colorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,23 @@ for(var i = 0; i < squares.length; i++){
         var clickedColor = this.style.backgroundColor;
 
         if(clickedColor === pickedColor) {
-            alert("Correct!");
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
         }else {
-            alert("WRONG!!!");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     })
+}
+
+function changeColors(color) {
+    //loop through all squares
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
